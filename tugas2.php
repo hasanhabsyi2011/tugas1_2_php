4a1de5cca8aed76cac6463209f57a27b20584ac5
hasanhabsyi2011 <71365610+hasanhabsyi2011@users.noreply.github.com>
2020-10-07 10:17:23 +0700

    Add files via upload

---
+++ tugas2.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+
+<?php 
+
+$cireng = 1000;
+$Sduren = 15000;
+$Smilo = 20000;
+$Steh = 5000;
+$piscok = 1000;
+
+
+?>
+
+
+
+<table border = 1>
+			<tr>
+				<th>No</td>
+				<th>Nama Menu</td>
+				<th>Harga</td>	
+			</tr>
+			<tr>
+				<td>1</td>
+				<td>Cireng</td>
+				<td>Rp <?php echo $cireng ?> </td>
+			</tr>
+			<tr>
+				<td>2</td>
+				<td>Es Duren</td>
+				<td>Rp <?php echo $Sduren ?> </td>
+			</tr>
+			<tr>
+				<td>3</td>
+				<td>Es Milo</td>
+				<td>Rp <?php echo $Smilo ?> </td>
+			</tr>
+			<tr>
+				<td>4</td>
+				<td>Es Teh</td>
+				<td>Rp <?php echo $Steh ?> </td>
+			</tr>
+			<tr>
+				<td>5</td>
+				<td>Piscok</td>
+				<td>Rp <?php echo $piscok ?> </td>
+			</tr>
+<table>
+
+
+
+
+
+</body>
+</html>
